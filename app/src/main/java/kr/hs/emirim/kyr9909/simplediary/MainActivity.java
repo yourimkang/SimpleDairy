7b4ba94362311eea26c52bc80aafbd755b83f3ff
Yurim Kang <kyr9909@naver.com>
2016-10-18 19:56:25 +0900

    #4 Event Programming

--- app/src/main/java/kr/hs/emirim/kyr9909/simplediary/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/kyr9909/simplediary/MainActivity.java
@@ -2,13 +2,22 @@ package kr.hs.emirim.kyr9909.simplediary;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Calendar;
+
+import android.content.Context;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,20 +25,24 @@ public class MainActivity extends AppCompatActivity {
     EditText editDiary;
     Button butSave;
     String fileName;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        datePic=(DatePicker)findViewById(R.id.date_picker);
-        editDiary=(EditText)findViewById(R.id.edit_content);
-        butSave=(Button)findViewById(R.id.but_save);
+        datePic = (DatePicker) findViewById(R.id.date_picker);
+        editDiary = (EditText) findViewById(R.id.edit_content);
+        butSave = (Button) findViewById(R.id.but_save);
 
         //현재 날짜 구하기
         Calendar calendar = Calendar.getInstance();
-        int nowYear=calendar.get(Calendar.YEAR);
-        int month=calendar.get(Calendar.MONTH);
-        int date=calendar.get(Calendar.DATE);
+        int nowYear = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int date = calendar.get(Calendar.DATE);
 
         //DatePicker에 현재 날짜 설정
         datePic.init(nowYear, month, date, new DatePicker.OnDateChangedListener() {
@@ -42,15 +55,34 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        butSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FileOutputStream out = null;
+                try {
+                    out = openFileOutput(fileName, Context.MODE_WORLD_WRITEABLE);
+                    String diaryContents=editDiary.getText().toString();
+                    out.write(diaryContents.getBytes());
+                    out.close();
+                    Toast.makeText(getApplicationContext(),"저장이 완료됨",Toast.LENGTH_SHORT).show();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
     }
+
     String readDiary(String fileName) {
-        String diaryContents=null;
+        String diaryContents = null;
         try {
-            FileInputStream in=openFileInput(fileName);
-            byte[] txt=new byte[500];
+            FileInputStream in = openFileInput(fileName);
+            byte[] txt = new byte[500];
             in.read(txt);
             in.close();
-            diaryContents=new String(txt);
+            diaryContents = new String(txt);
             butSave.setText("수정 하기");
 
         } catch (IOException e) {
